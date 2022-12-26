bba71e8f1f895580272a774481c66910864f7cd7
PinYu1618 <pinyuchen658503@gmail.com>
2022-12-26 20:18:13 +0800

    fix: escape char

--- src/components/game-card.tsx
+++ src/components/game-card.tsx
@@ -12,7 +12,7 @@ export function GameCard({ title, href, description }: GameCardProps) {
   return (
     <li>
       {/*<Image src={thumbnail} alt='' width={300} height={300} />*/}
-      <Link href={href}>{title}'s thumbnail</Link>
+      <Link href={href}>{title} thumbnail</Link>
       <div>
         <h3>{title}</h3>
         <p>{description}</p>
